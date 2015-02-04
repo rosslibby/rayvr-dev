8043caf20b6d7d2c8119a0179ea8c4b333516805
Ross Libby <libtheross@gmail.com>
2015-02-04 13:27:09 -0500

    Removed "How it works" navigation item temporarily 	modified:   app/views/includes/navigation.blade.php

--- app/views/includes/navigation.blade.php
+++ app/views/includes/navigation.blade.php
@@ -3,7 +3,6 @@
 </div>
 <ul class="nav navbar-nav navbar-right">
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('/') ? 'active' : '' }}"><a href="/">HOME</a></li>
-	<li class="fg-scheme-dark-gray anchor {{ Request::is('about') ? 'active' : '' }}"><a href="/about">HOW IT WORKS</a></li>
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('register') ? 'active' : '' }}"><a href="/register">REGISTER</a></li>
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('contact') ? 'active' : '' }}"><a href="/contact">CONTACT</a></li>
 </ul>
