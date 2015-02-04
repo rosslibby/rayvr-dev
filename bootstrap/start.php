c9300b34930e0cabfaea0009791fda0a9da4fb4b
Ross Libby <libtheross@gmail.com>
2015-02-04 09:16:07 -0500

    modified:   bootstrap/start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('localhost'),
-	'production' => array('rayvr.com')
+	'production' => array('104.236.204.234')
 
 ));
 
