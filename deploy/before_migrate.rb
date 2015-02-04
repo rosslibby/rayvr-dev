6c749f317e8e091bddbf4f0f0ad54ca54cdd84fb
Ross Libby <libtheross@gmail.com>
2015-02-03 23:06:44 -0500

    Add before_migrate.rb for opsworks

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,21 @@
+[release_path].each do |path|
+    log "before_migrate hook [#{path}]" do
+        level :debug
+    end
+    # run composer install
+    execute "composer install" do
+        user "deploy"
+        cwd path
+        command "composer install --no-dev --no-interaction --optimize-autoloader"
+        # only execute for composer projects
+        only_if "test -f \"#{path}/composer.json\""
+    end
+    # correct permissions to allow apache to write
+    execute "chown #{path}/app/storage" do
+        cwd "#{path}/app/storage"
+        command "chown -R deploy.www-data ."
+    end
+    execute "chmod #{path}/app/storage" do
+        cwd "#{path}/app/storage"
+        command "chmod -R u+rwX,g+rwX ."
+    end
