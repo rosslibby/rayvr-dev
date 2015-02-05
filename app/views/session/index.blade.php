0e177f803ca81870e6c31d4f3ebde02d249bdf12
Ross Libby <libtheross@gmail.com>
2015-02-05 17:06:06 -0500

    Updated business page to have responsive Vimeo embed (Thank you embedresponsively.com!) 	modified:   app/views/session/index.blade.php

--- app/views/session/index.blade.php
+++ app/views/session/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.landing-master')
 
 @section('video')
-<iframe src="//player.vimeo.com/video/118839151" width="1000" height="562" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://player.vimeo.com/video/118839151' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
 @stop
 
 @section('content')
