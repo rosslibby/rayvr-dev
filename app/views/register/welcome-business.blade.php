49228ce8b1f6da9ef1b0b85261adaad151e2ee57
Ross Libby <libtheross@gmail.com>
2015-02-05 00:29:31 -0500

    added the new view yayyy

---
+++ app/views/register/welcome-business.blade.php
@@ -0,0 +1,67 @@
+@include('includes.head')
+
+			<div class="early-signup-header-bg">
+				<div class="container">
+
+					<div class="row text-center">
+						<h3 class="h3-5 fg-scheme-dark">Targeted Product Marketing</h3>
+					</div>
+
+					<br>
+
+					<div class="row text-center">
+						<div class="col-md-6 col-md-offset-3">
+							<div class="row">
+								<div class="col-md-10 col-md-offset-1">
+									<p class="larger lighter">RAYVR connects your products with users who love to write rave reviews.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<br>
+
+				</div>
+			</div>
+
+
+			<br>
+			<br>
+			<br>
+			<br>
+
+			<div class="container text-center">
+				<h3>We'll be in touch!</h3>
+			</div>
+
+
+			<div class="container">
+
+				<div class="col-lg-9 col-lg-offset-2">
+					<div class="col-md-12">
+						<div class="alert alert-success lighter col-sm-11">
+							<strong>You're all set! </strong>Congratulations, you will be one of the first to know when RAYVR goes live. See you soon!
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+
+			<div class="container">
+				<div class="row text-center">
+					{{ HTML::image('resources/img/logo.png') }}
+				</div>
+			</div>
+
+			<br>
+			<br>
+			<br>
+
+@include('includes.foot')
