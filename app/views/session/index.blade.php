247ce2b40bc490bc453a3897546879a19434e548
Ross Libby <libtheross@gmail.com>
2015-02-09 16:19:15 -0500

    ...and again, trying to make the form look good on smaller screens 	modified:   app/views/session/index.blade.php

--- app/views/session/index.blade.php
+++ app/views/session/index.blade.php
@@ -116,14 +116,14 @@ Before we send an offer out to our members, we have it tested for quality by our
 <div class="business-registration-container row-dark-blue">
 	<div class="col-lg-4 col-md-6 col-sm-8 col-lg-offset-4 col-md-offset-3 col-sm-offset-2">
 		<div class="row text-center">
-			<div class="col-md-10 col-md-offset-1">
+			<div class="col-lg-10 col-md-12 col-lg-offset-1">
 				<h3 class="business-registration">Register Your Business</h3>
 			</div>
 		</div>
 		<br>
 		<br>
 		<div class="row">
-			<div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+			<div class="col-lg-10 col-lg-offset-1 col-md-12">
 				@include('forms.register.inset-business')
 				@if(Session::has('error'))
 					<br>
