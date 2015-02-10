c3a548ea56cb83927abe9ee2e6d256acc4e71b53
Ross Libby <libtheross@gmail.com>
2015-02-10 14:15:18 -0500

    Add the matches queue migration 	new file:   app/database/migrations/2015_02_10_191437_create_matches_table.php

---
+++ app/database/migrations/2015_02_10_191437_create_matches_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMatchesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('matches', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->integer('business_id');
+			$table->integer('offer_id');
+			$table->integer('category_id');
+			$table->timestamp('live');
+			$table->boolean('accept');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('matches');
+	}
+
+}
