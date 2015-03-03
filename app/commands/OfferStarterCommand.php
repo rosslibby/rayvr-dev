9998577db3012fef7f2a0faf6df194f16b8c56d3
ROSS LIBBY <libtheross@gmail.com>
2015-03-03 16:25:46 -0500

    Changed offer starter command to run at hour 22 (UTC) 	modified:   OfferStarterCommand.php

--- app/commands/OfferStarterCommand.php
+++ app/commands/OfferStarterCommand.php
@@ -42,7 +42,7 @@ class OfferStarterCommand extends ScheduledCommand {
 	 */
 	public function schedule(Schedulable $scheduler)
 	{
-		return $scheduler->daily()->hours(15);
+		return $scheduler->daily()->hours(22);
 	}
 
 	/**
