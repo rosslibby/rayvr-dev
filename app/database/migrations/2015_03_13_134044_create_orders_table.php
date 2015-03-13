b17f18ad289a32a4f7f38768ba96c52766ab40e5
ROSS LIBBY <libtheross@gmail.com>
2015-03-13 09:47:54 -0400

    New orders migration 	new file:   app/database/migrations/2015_03_13_134044_create_orders_table.php

---
+++ app/database/migrations/2015_03_13_134044_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('offer_id');
+			$table->integer('user_id');
+			$table->string('code');
+			$table->timestamp('mail_date');
+			$table->string('confirmation_number');
+			$table->integer('shipping_claim');
+			$table->boolean('reimbursed')->default(false);
+			$table->boolean('disputed')->default(false);
+			$table->boolean('paid_shipping')->default(false);
+			$table->float('cost');
+			$table->text('review');
+			$table->timestamp('reviewed');
+			$table->boolean('completed')->default(false);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
