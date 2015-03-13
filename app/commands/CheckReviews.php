38674d9b3c1f4a0b280df4122ef5a1f06ff3484e
ROSS LIBBY <libtheross@gmail.com>
2015-03-13 09:00:42 -0400

    Added review-checking bot Added contact form following the dark-form convention that I made up for the registration pages Began working on credit-card-number-hyphenating script for Stripe forms 	new file:   app/commands/CheckReviews.php 	new file:   app/views/forms/contact/inset-contact.blade.php 	new file:   app/views/forms/payment/sample.blade.php

---
+++ app/commands/CheckReviews.php
@@ -0,0 +1,85 @@
+<?php
+
+use Indatus\Dispatcher\Scheduling\ScheduledCommand;
+use Indatus\Dispatcher\Scheduling\Schedulable;
+use Indatus\Dispatcher\Drivers\Cron\Scheduler;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class CheckReviews extends ScheduledCommand {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'reviews:verify';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Verify that reviews exist.';
+	protected $offer;
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(RAYVR\Storage\Offer\OfferRepository $offer)
+	{
+		
+		$this->offer = $offer;
+		parent::__construct();
+	}
+
+	/**
+	 * When a command should run
+	 *
+	 * @param Scheduler $scheduler
+	 * @return \Indatus\Dispatcher\Scheduling\Schedulable
+	 */
+	public function schedule(Schedulable $scheduler)
+	{
+		return $scheduler->everyMinute();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$this->line('Verifying reviews...');
+		$this->offer->verifyReview();
+		$this->line('The reviews have been verified.');
+		Log::info('Review verification completed.');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [
+			['example', InputArgument::OPTIONAL, 'An example argument.'],
+		];
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+		);
+	}
+}
