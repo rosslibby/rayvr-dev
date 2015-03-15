45de6c5e4a825b546ca3165de3d351b6f8a308f6
ROSS LIBBY <libtheross@gmail.com>
2015-03-15 11:45:13 -0400

    Built user-faq page 	new file:   app/views/resources/user-faq.blade.php

---
+++ app/views/resources/user-faq.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.policies')
+@section('policy')
+<h3>FAQ For Users</h3>
+<br>
+<p class="h5">How does RAYVR work??</p>
+<p>Businesses want to get great reviews for their products. They’re willing to give you products that match your interests for free, and even pay for shipping in hopes that you will love the product and write a great review.</p>
+<br>
+<p class="h5">Is there any cost to join RAYVR?</p>
+<p>No, RAYVR is completely free.</p>
+<br>
+<p class="h5">How do I get my first product offer?</p>
+<p>You first product offer will be available when you login to your RAYVR account. If an offer is not currently available, please wait until you receive a notification email</p>
+<br>
+<p class="h5">Can I do back to get a product that I denied?</p>
+<p>No, you cannot go back to view a product that you denied.</p>
+<br>
+<p class="h5">What if the product doesn’t work?
+<p>If you have an issue with a product ordered through RAYVR, simply login and fill out a feedback survey so we can notify the business and help them improve the product.</p>
+<br>
+<p class="h5">How long do I have to give feedback?</p>
+<p>We ask that you give feedback on a product as soon as it arrives and you test it out. Businesses give free product offers through RAYVR hoping to get great reviews for their products. We ask that you review all products within two weeks of ordering.</p>
+<br>
+<p class="h5">Can I get more than one product from RAYVR at a time?</p>
+<p>No, RAYVR users can only redeem one offer at a time.</p>
+<br>
+<p class="h5">How am I reimbursed for shipping?</p>
+<p>If you claim a shipping amount, it will be verified and you will be reimbursed for your shipping 48 hours after ordering the product. You will be reimbursed via PayPal and receive an email with your reimbursement information. You must have a PayPal account to be reimbursed.</p>
+<br>
+<p class="h5">How do I cancel my account?</p>
+<p>To cancel your account, go to the Account Preferences page and click Deactivate account at the bottom. Once deactivated, you must contact {{ HTML::link('mailto:support@rayvr.com', 'support@rayvr.com') }} to reactivate your account.</p>
+<br>
+<p class="h5">How do I get RAYVR points?</p>
+<p>You score points for involvement, speed, and inviting your friends. Users with the highest points will be deemed most qualified to receive the best new offers.</p>
+<br>
+<p class="h5">Where is RAYVR currently available?</p>
+<p>RAYVR is currently only available in the US.</p>
+@stop
