1e1b2dd5360df5460f1954532ae34d7d30d674ab
ROSS LIBBY <libtheross@gmail.com>
2015-03-15 10:59:45 -0400

    Billing fix (try 6) 	modified:   app/lib/RAYVR/Storage/User/EloquentUserRepository.php

--- app/lib/RAYVR/Storage/User/EloquentUserRepository.php
+++ app/lib/RAYVR/Storage/User/EloquentUserRepository.php
@@ -643,7 +643,7 @@ class EloquentUserRepository implements UserRepository {
 		 */
 		if($response->paid && !$response->refunded)
 		{
-			$billing = Billing::where('stripe_id', $card->id)->first();
+			$billing = Billing::where('stripe_id', $card)->first();
 			$billing->verified = true;
 			$billing->save();
 
