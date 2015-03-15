69b11f63d921b957a118b485fbb52bbba39dd4a4
ROSS LIBBY <libtheross@gmail.com>
2015-03-15 18:04:16 -0400

    Add the business faq 	new file:   app/views/resources/business-faq.blade.php

---
+++ app/views/resources/business-faq.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.policies')
+@section('policy')
+<h3>FAQ For Business</h3>
+<br>
+<p class="h5">Does this break any rules?</p>
+<p>No, RAYVR connects you with real people who are genuinely interested in your product and give honest feedback. They are not paid or coerced to write a review.</p>
+<br>
+<p class="h5">How do shipping reimbursements work?</p>
+<p>If a product qualifies for free shipping, or is eligible for Prime shipping and ordered by a Prime member, there is no shipping charge. To ensure maximum user engagement, we reimburse users for shipping costs, you will be charged at the end of your promotion for all shipping reimbursements made for your products.</p>
+<br>
+<p class="h5">How will a business know a person wrote a review?</p>
+<p>We track reviews and feedback in your business dashboard.</p>
+<br>
+<p class="h5">What happens if a person doesn't review my offer?</p>
+<p>If a person has an issue with your product, we give them the option to take a feedback survey instead of writing a review.</p>
+<br>
+<p class="h5">Can someone leave a bad review for my product?<p>
+<p>We encourage users who leave reviews to be honest and relay their experience with your product. They are also required to disclose that they received the product for free in exchange for writing a review. Most people will choose to fill out a feedback survey instead of a review for products they were unhappy with.</p>
+<br>
+<p class="h5">What happens if my product gets too much bad feedback?</p>
+<p>We terminate promotions for products with too much bad feedback to ensure a great experience for our users.</p>
+<br>
+<p class="h5">Are people compensated for leaving reviews?</p>
+<p>No, they are not compensated for leaving reviews. RAYVR is free to users and they receive noting in return but the free product.</p>
+<br>
+<p class="h5">Can I end a promotion early?</p>
+<p>To end a promotion early, contact {{ HTML::link('mailto:support@rayvr.com', 'support@rayvr.com') }}</p>
+<br>
+<p class="h5">How do I cancel my account?</p>
+<p>Contact {{ HTML::('mailto:support@rayvr.com', 'support@rayvr.com') }}.</p>
+<br>
+<p class="h5">What products are not allowed on RAYVR?</p>
+<p>Vitamins, supplements, e-books, books, food products, and adult products are not allowed on RAYVR. To keep a high level of quality for products in our program, we reserve the right to reject any product submitted at our discretion.</p>
+<br>
+<p class="h5">How am I charged for using RAYVR?</p>
+<p>When you begin a new promotion, you authorize a maximum budget for the promotion. When the promotion is complete, your credit card will be charged for each time someone confirms that they ordered your product with a promo code and for any shipping reimbursements.</p>
+@stop
