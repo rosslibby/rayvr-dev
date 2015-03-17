b3006964aa2d51cf40d1cb419d12d0c1c4988114
ROSS LIBBY <libtheross@gmail.com>
2015-03-17 12:54:14 -0400

    Added a chron job to run the secondMatching every minute 	new file:   app/commands/SecondMatching.php 	new file:   app/database/migrations/2015_03_17_164636_add_billing_to_orders.php

---
+++ app/commands/SecondMatching.php
@@ -0,0 +1,85 @@
+<?php
+
+use Indatus\Dispatcher\Scheduling\ScheduledCommand;
+use Indatus\Dispatcher\Scheduling\Schedulable;
+use Indatus\Dispatcher\Drivers\Cron\Scheduler;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class SecondMatching extends ScheduledCommand {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'promotions:match';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Match promotions and users';
+	protected $offer;
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(RAYVR\Storage\Offer\OfferRepository $offer)
+	{
+		
+		$this->offer = $offer;
+		parent::__construct();
+	}
+
+	/**
+	 * When a command should run
+	 *
+	 * @param Scheduler $scheduler
+	 * @return \Indatus\Dispatcher\Scheduling\Schedulable
+	 */
+	public function schedule(Schedulable $scheduler)
+	{
+		return $scheduler;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$this->line('Matching users to promotions');
+		$this->offer->secondMatching();
+		$this->line('The matches have been made');
+		Log::info('Users matched with promotions.');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [
+			['example', InputArgument::OPTIONAL, 'An example argument.'],
+		];
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+		);
+	}
+}
