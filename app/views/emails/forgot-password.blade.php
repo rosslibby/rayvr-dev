2e51fbb2eafa8e05f1521b6185761e4efde4c2b7
ROSS LIBBY <libtheross@gmail.com>
2015-03-18 17:10:55 -0400

    Modified all the emails to pull from a template. Much better this way. 	modified:   app/views/emails/business-shipping-dispute-filed.blade.php 	modified:   app/views/emails/business-welcome.blade.php 	modified:   app/views/emails/forgot-password.blade.php 	modified:   app/views/emails/invite-user.blade.php 	modified:   app/views/emails/new-offer.blade.php 	modified:   app/views/emails/offer-approved.blade.php 	modified:   app/views/emails/offer-completed.blade.php 	modified:   app/views/emails/offer-denied.blade.php 	modified:   app/views/emails/offer-match.blade.php 	modified:   app/views/emails/offer-submitted.blade.php 	modified:   app/views/emails/order-reminder.blade.php 	modified:   app/views/emails/review-reminder.blade.php

--- app/views/emails/forgot-password.blade.php
+++ app/views/emails/forgot-password.blade.php
@@ -1,9 +1,26 @@
-<h1>{{ $name }},</h1>
-<p><strong>We received a request to reset your password.</strong></p>
-<p>If you believe you have received this message in error, please take no action.</p>
-<p>If you wish to reset your password, please use the following link: <a href="{{ $link }}">{{ $link }}</a></p>
+@extends('emails.email')
+
+@section('heading')
+Password 
+@stop
+
+@section('heading-accent')
+Reset
+@stop
+
+@section('greeting')
+Hi {{ $name }},
+@stop
+
+@section('body')
+<strong>We received a request to reset your password.</strong>
+If you believe you have received this message in error, please take no action.
+<br>
+If you wish to reset your password, please use the following link: <a href="{{ $link }}">{{ $link }}</a>
 <br>
 <br>
-<p>- <em>RAYVR Support</em></p>
+<em>RAYVR Support</em>
 <br>
-<p><a href="mailto:support@rayvr.com">support@rayvr.com</a></p>
+<br>
+<a href="mailto:support@rayvr.com">support@rayvr.com</a>
+@stop
---
+++ app/views/emails/invite-user.blade.php
@@ -0,0 +1,13 @@
+@extends('emails.email')
+
+@section('heading')
+@stop
+
+@section('heading-accent')
+@stop
+
+@section('greeting')
+@stop
+
+@section('body')
+@stop
