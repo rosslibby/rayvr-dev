cbc9d889b8f097f8486bce1243c0ca18ae22843f
ROSS LIBBY <libtheross@gmail.com>
2015-03-20 14:47:56 -0400

    changed env to all caps 	modified:   app/lib/RAYVR/Storage/Offer/EloquentOfferRepository.php

--- app/lib/RAYVR/Storage/Offer/EloquentOfferRepository.php
+++ app/lib/RAYVR/Storage/Offer/EloquentOfferRepository.php
@@ -23,7 +23,7 @@ class EloquentOfferRepository implements OfferRepository {
 		$gateway->setApiKey('sk_test_3YmCSPqFkZCBhSroMCu4QAC0');
 		$this->gateway = $gateway;
 
-		Stripe::setApiKey(getenv['stripe_api_key']);
+		Stripe::setApiKey($_ENV['STRIPE_API_KEY']);
 	}
 
 	public function all()
