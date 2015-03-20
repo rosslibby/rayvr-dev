002e1382d7be93c1ac093a9eacf8ac21f9bc6103
ROSS LIBBY <libtheross@gmail.com>
2015-03-20 14:41:20 -0400

    env test 3 	modified:   bootstrap/start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment([
 
-	'local' => ['localhost'],
+	'local' => ['production'],
 	'staging' => ['staging'],
 	'production' => ['production']
 
