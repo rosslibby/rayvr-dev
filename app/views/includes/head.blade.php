40ddcdee897b05569679cf3726e23c1e8c8676ab
ROSS LIBBY <libtheross@gmail.com>
2015-03-21 11:46:03 -0400

    Viewport for responsivity 	modified:   app/views/includes/head.blade.php

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -6,6 +6,8 @@
 	<!-- Load styles -->
 	@include('includes.styles')
 
+	<!-- Viewport -->
+	<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
 	<!-- For AJAX posting -->
 	<meta name="_token" content="{{ csrf_token() }}" />
 </head>
