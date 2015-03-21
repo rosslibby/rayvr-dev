ebd552b019ef0218af0d5b94138905f681faca92
ROSS LIBBY <libtheross@gmail.com>
2015-03-21 16:24:42 -0400

    killed selector plugin 	modified:   app/views/includes/dash-foot.blade.php

--- app/views/includes/dash-foot.blade.php
+++ app/views/includes/dash-foot.blade.php
@@ -2,9 +2,7 @@
 {{ HTML::script( 'resources/js/jquery.min.js' ) }}
 {{ HTML::script( 'resources/js/bootstrap.min.js' ) }}
 {{ HTML::script( 'resources/js/icheck.min.js' ) }}
-{{ HTML::script( 'resources/js/jquery.fs.selecter.min.js' ) }}
 {{ HTML::script( 'resources/js/jquery.fs.stepper.min.js' ) }}
-{{ HTML::script( 'resources/js/jquery.fs.selecter.js' ) }}
 {{ HTML::script( 'resources/js/bootstrap-datepicker.js' ) }}
 {{ HTML::script( 'resources/js/bootstrap-slider.js' ) }}
 {{ HTML::script( 'resources/js/bootstrap-rating-input.js' ) }}
@@ -192,9 +190,6 @@ $(document).ready(function(){
 			}
 		},"json");
 	});
-	
-	/** Selecter **/
-	// $("select").selecter();
 
 	/** Date range **/
 	var date = new Date();
