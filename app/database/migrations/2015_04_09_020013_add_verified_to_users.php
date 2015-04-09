830b2c09e902591abb5e25649625b98a70270b82
ROSS LIBBY <libtheross@gmail.com>
2015-04-08 23:29:55 -0400

    Added "verified" boolean field to users table 	new file:   app/database/migrations/2015_04_09_020013_add_verified_to_users.php

---
+++ app/database/migrations/2015_04_09_020013_add_verified_to_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddVerifiedToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->boolean('verified')->default(false);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('verified');
+		});
+	}
+
+}
