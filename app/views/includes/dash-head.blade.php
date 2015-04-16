44a355430ce29ff7aa0047f89f6d45944f46b09d
ROSS LIBBY <libtheross@gmail.com>
2015-04-15 20:59:21 -0400

    Added google analytics 	modified:   app/views/includes/dash-head.blade.php 	modified:   app/views/includes/head.blade.php 	modified:   app/views/includes/user-dash-head.blade.php

--- app/views/includes/dash-head.blade.php
+++ app/views/includes/dash-head.blade.php
@@ -27,6 +27,18 @@
 		var stripe_publishable_key = "{{ $_ENV['stripe_publishable_key'] }}";
 		Stripe.setPublishableKey(stripe_publishable_key);
 	</script>
+
+	<!-- GA -->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-61914678-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
 </head>
 <body>
 <nav class="navbar navbar-primary navbar-fixed-top bg-scheme-white navbar-collapse collapse">
--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<title>Sign Up with RAYVR for Early Access</title>
+	<title>Get Free Stuff with RAYVR | Quality Products for Free | Try Products and Keep Them</title>
 
 	<!-- Load styles -->
 	@include('includes.styles')
@@ -10,6 +10,18 @@
 	<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
 	<!-- For AJAX posting -->
 	<meta name="_token" content="{{ csrf_token() }}" />
+
+	<!-- GA -->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-61914678-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
 </head>
 <body>
 	<div class="wrapper">
