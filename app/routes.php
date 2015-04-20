59f48158c9c8a2877c7546a502d06e5fcdd3e3d4
ROSS LIBBY <libtheross@gmail.com>
2015-04-20 09:45:44 -0400

    Fixed business routes 	modified:   app/routes.php

--- app/routes.php
+++ app/routes.php
@@ -35,6 +35,96 @@ Route::group(['before' => 'csrf'], function()
 		Route::get('/', function(){
 			return Redirect::to('business');
 		});
+
+		/**
+		 * Business-specific routes
+		 */
+
+		Route::group(['before' => 'business'], function(){
+
+			/**
+			 * Business FAQ
+			 */
+			Route::get('business/faq', function(){
+				return View::make('resources.business-faq');
+			});
+
+			/**
+			 * Dispute shipping claim
+			 */
+			Route::get('orders/{order}/shipping', function($order){
+				$order = Order::find($order);
+				return View::make('orders.shipping-claim')->with('claim', $order->reimbursement);
+			});
+			Route::post('orders/{order}/shipping', 'OrderController@dispute');
+
+			/**
+			 * Delete credit card
+			 */
+			Route::get('card/{id}/delete', [
+				'uses' => 'PaymentController@delete',
+				'as' => 'card.delete'
+			]);
+
+			/**
+			 * Offers ordered
+			 */
+			Route::get('offers/track/{id}', [
+				'uses' => 'OffersController@single',
+				'as' => 'offers.single'
+			]);
+
+			/**
+			 * Image uploader
+			 */
+			Route::get('offer/image', function(){
+				return View::make('offers.image-upload');
+			});
+			Route::post('offer/image', function(){
+				$file = Input::file('image');
+				$extension = ".".$file->getClientOriginalExtension();
+				$destinationPath = public_path().'/uploads/';
+				$filename = "OfferTitle-".str_random(14)."-".microtime(true).$extension;
+				Input::file('image')->move($destinationPath, $filename);
+			});
+
+			/**
+			 * Shipping document uploader
+			 */
+			Route::get('order/{order}/dispute', 'OrderController@shippingDoc');
+			Route::post('order/{order}/dispute', 'OrderController@shippingSave');
+
+			/**
+			 * Offer-pack routes
+			 */
+			Route::get('offers/purchase', 'PaymentController@offers');
+			Route::post('offers/purchase', [
+				'uses' => 'PaymentController@payForOffers',
+				'as' => 'offers/purchase'
+			]);
+
+			/**
+			 * Billing portal
+			 */
+			Route::get('billing', [
+				'uses' => 'PaymentController@billing',
+				'as' => 'billing'
+			]);
+			Route::post('billing', [
+				'uses' => 'PaymentController@subscribe',
+				'as' => 'payments'
+			]);
+
+			/**
+			 * Pay shipping deposit
+			 */
+			Route::get('offers/track/{id}/deposit', 'OffersController@deposit');
+
+			/**
+			 * Process shipping deposit
+			 */
+			Route::get('offers/shipping/deposit', 'OffersController@processDeposit');
+		});
 	});
 
 	/**
@@ -162,96 +252,6 @@ Route::group(['before' => 'csrf'], function()
 			 */
 			Route::get('offers/delete/{id}', 'OffersController@deletePromo');
 		});
-	});
-
-	/**
-	 * Business-specific routes
-	 */
-
-	Route::group(['before' => 'business'], function(){
-
-		/**
-		 * Business FAQ
-		 */
-		Route::get('business/faq', function(){
-			return View::make('resources.business-faq');
-		});
-
-		/**
-		 * Dispute shipping claim
-		 */
-		Route::get('orders/{order}/shipping', function($order){
-			$order = Order::find($order);
-			return View::make('orders.shipping-claim')->with('claim', $order->reimbursement);
-		});
-		Route::post('orders/{order}/shipping', 'OrderController@dispute');
-
-		/**
-		 * Delete credit card
-		 */
-		Route::get('card/{id}/delete', [
-			'uses' => 'PaymentController@delete',
-			'as' => 'card.delete'
-		]);
-
-		/**
-		 * Offers ordered
-		 */
-		Route::get('offers/track/{id}', [
-			'uses' => 'OffersController@single',
-			'as' => 'offers.single'
-		]);
-
-		/**
-		 * Image uploader
-		 */
-		Route::get('offer/image', function(){
-			return View::make('offers.image-upload');
-		});
-		Route::post('offer/image', function(){
-			$file = Input::file('image');
-			$extension = ".".$file->getClientOriginalExtension();
-			$destinationPath = public_path().'/uploads/';
-			$filename = "OfferTitle-".str_random(14)."-".microtime(true).$extension;
-			Input::file('image')->move($destinationPath, $filename);
-		});
-
-		/**
-		 * Shipping document uploader
-		 */
-		Route::get('order/{order}/dispute', 'OrderController@shippingDoc');
-		Route::post('order/{order}/dispute', 'OrderController@shippingSave');
-
-		/**
-		 * Offer-pack routes
-		 */
-		Route::get('offers/purchase', 'PaymentController@offers');
-		Route::post('offers/purchase', [
-			'uses' => 'PaymentController@payForOffers',
-			'as' => 'offers/purchase'
-		]);
-
-		/**
-		 * Billing portal
-		 */
-		Route::get('billing', [
-			'uses' => 'PaymentController@billing',
-			'as' => 'billing'
-		]);
-		Route::post('billing', [
-			'uses' => 'PaymentController@subscribe',
-			'as' => 'payments'
-		]);
-
-		/**
-		 * Pay shipping deposit
-		 */
-		Route::get('offers/track/{id}/deposit', 'OffersController@deposit');
-
-		/**
-		 * Process shipping deposit
-		 */
-		Route::get('offers/shipping/deposit', 'OffersController@processDeposit');
 	});
 
 	/**
