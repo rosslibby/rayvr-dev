28328588c5d0960b96a5765f1a955934cb1ebf02
ROSS LIBBY <libtheross@gmail.com>
2015-05-12 09:36:59 -0400

    Modified nav to reflect correct nav items 	modified:   app/views/includes/admin-nav.blade.php

--- app/views/includes/admin-nav.blade.php
+++ app/views/includes/admin-nav.blade.php
@@ -4,7 +4,7 @@
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('users') ? 'active' : '' }}"><a href="/users">Users</a></li>
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('affiliates') ? 'active' : '' }}"><a href="/affiliates">Affiliates</a></li>
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('offers/all') ? 'active' : '' }}"><a href="/offers/all">All promotions <span class="badge badge-info">{{ count(Offer::all()) }}</span></a></li>
-	<li class="fg-scheme-dark-gray anchor {{ Request::is('offers/moderate') ? 'active' : '' }}"><a href="/offers/moderate">New promotions <span class="badge badge-danger">{{ count(Offer::where('approved',false)->get()) }}</span></a></li>
+	<li class="fg-scheme-dark-gray anchor {{ Request::is('offers/moderate') ? 'active' : '' }}"><a href="/offers/moderate">New promotions <span class="badge badge-danger">{{ count(Offer::where(['approved' => false, 'reason' => ''])->get()) }}</span></a></li>
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('offers/active') ? 'active' : '' }}"><a href="/offers/active">Active promotions <span class="badge badge-success">{{ count(Offer::where('approved',true)->where('start','<=',date('Y-m-d'))->where('end', '>=', date('Y-m-d'))->get()) }}</span></a></li>
 	<li class="fg-scheme-dark-gray anchor {{ Request::is('shipping/moderate') ? 'active' : '' }}"><a href="/shipping/moderate">Shipping claims <span class="badge badge-warning">{{ count(Reimbursement::where('resolved',false)->get()) }}</span></a></li>
 	{{-- <li class="fg-scheme-dark-gray anchor {{ Request::is('discounts') ? 'active' : '' }}"><a href="/discounts">Discounts</a></li> --}}
