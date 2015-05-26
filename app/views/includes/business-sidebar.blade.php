23442bbc7098bafebe556c1206bc9029fc843ba5
ROSS LIBBY <libtheross@gmail.com>
2015-05-26 10:31:00 -0400

    Moved /billing menu item down on sidebar 	modified:   app/views/includes/business-sidebar.blade.php

--- app/views/includes/business-sidebar.blade.php
+++ app/views/includes/business-sidebar.blade.php
@@ -5,11 +5,6 @@
 @if(!empty(json_decode($offers)))
 <li class="{{ Request::is('offers/track*') ? 'active' : '' }}"><a href="/offers/track"><span class="glyphicon glyphicon-stats"></span>&nbsp;&nbsp;&nbsp;&nbsp;Track Offers</a></li>
 @endif
-
-{{-- If the preferences have been completed, show the "billing" form --}}
-@if(Auth::user()->first_name && Auth::user()->last_name && Auth::user()->email && Auth::user()->address && Auth::user()->city && Auth::user()->country && Auth::user()->zip && Auth::user()->phone)
-	<li class="{{ Request::is('billing') ? 'active' : '' }}"><a href="/billing"><span class="glyphicon glyphicon-credit-card"></span>&nbsp;&nbsp;&nbsp;&nbsp;Billing</a></li>
-@endif
 @if(Auth::user()->stripe_customer)
 	{{-- @if(!empty(json_decode($offers))) --}}
 		<li class="{{ Request::is('offers/add') ? 'active' : '' }}"><a href="/offers/add"><span class="glyphicon glyphicon-tag"></span>&nbsp;&nbsp;&nbsp;&nbsp;New Promotion</a></li>
@@ -17,4 +12,8 @@
 @endif
 {{-- Always show the Preferences and Support options --}}
 <li class="{{ Request::is('settings') ? 'active' : '' }}"><a href="/settings"><span class="glyphicon glyphicon-wrench"></span>&nbsp;&nbsp;&nbsp;&nbsp;Preferences</a></li>
+{{-- If the preferences have been completed, show the "billing" form --}}
+@if(Auth::user()->first_name && Auth::user()->last_name && Auth::user()->email && Auth::user()->address && Auth::user()->city && Auth::user()->country && Auth::user()->zip && Auth::user()->phone)
+	<li class="{{ Request::is('billing') ? 'active' : '' }}"><a href="/billing"><span class="glyphicon glyphicon-credit-card"></span>&nbsp;&nbsp;&nbsp;&nbsp;Billing</a></li>
+@endif
 <li><a href="/business-contact"><i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp;Support</a></li>
