8758f40b174acbedc30c0a0bd1cc3ec9faf90a5e
ROSS LIBBY <libtheross@gmail.com>
2015-05-27 10:37:52 -0400

    Made business sidebar reflect active page, changed from glyphicons to fontawesome in sidebar 	modified:   app/views/includes/business-sidebar.blade.php

--- app/views/includes/business-sidebar.blade.php
+++ app/views/includes/business-sidebar.blade.php
@@ -3,17 +3,17 @@
 
 {{-- If the business has any approved offers, show the entire menu --}}
 @if(!empty(json_decode($offers)))
-<li class="{{ Request::is('offers/track*') ? 'active' : '' }}"><a href="/offers/track"><span class="glyphicon glyphicon-stats"></span>&nbsp;&nbsp;&nbsp;&nbsp;Track Promotions</a></li>
+<li class="{{ Request::is('promotions/track*') ? 'active' : '' }}"><a href="/offers/track"><i class="fa fa-bar-chart"></i>&nbsp;&nbsp;&nbsp;&nbsp;Track Promotions</a></li>
 @endif
 @if(Auth::user()->stripe_customer)
 	{{-- @if(!empty(json_decode($offers))) --}}
-		<li class="{{ Request::is('offers/add') ? 'active' : '' }}"><a href="/offers/add"><span class="glyphicon glyphicon-tag"></span>&nbsp;&nbsp;&nbsp;&nbsp;New Promotion</a></li>
+		<li class="{{ Request::is('promotions/new') ? 'active' : '' }}"><a href="/offers/add"><i class="fa fa-tag"></i>&nbsp;&nbsp;&nbsp;&nbsp;New Promotion</a></li>
 	{{-- @endif --}}
 @endif
 {{-- Always show the Preferences and Support options --}}
-<li class="{{ Request::is('settings') ? 'active' : '' }}"><a href="/settings"><span class="glyphicon glyphicon-wrench"></span>&nbsp;&nbsp;&nbsp;&nbsp;Preferences</a></li>
+<li class="{{ Request::is('settings') ? 'active' : '' }}"><a href="/settings"><i class="fa fa-wrench"></i>&nbsp;&nbsp;&nbsp;&nbsp;Preferences</a></li>
 {{-- If the preferences have been completed, show the "billing" form --}}
 @if(Auth::user()->first_name && Auth::user()->last_name && Auth::user()->email && Auth::user()->address && Auth::user()->city && Auth::user()->country && Auth::user()->zip && Auth::user()->phone)
-	<li class="{{ Request::is('billing') ? 'active' : '' }}"><a href="/billing"><span class="glyphicon glyphicon-credit-card"></span>&nbsp;&nbsp;&nbsp;&nbsp;Billing</a></li>
+	<li class="{{ Request::is('billing') ? 'active' : '' }}"><a href="/billing"><i class="fa fa-credit-card"></i>&nbsp;&nbsp;&nbsp;&nbsp;Billing</a></li>
 @endif
-<li><a href="/business-contact"><i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp;Support</a></li>
+<li class="{{ Request::is('business-contact') ? 'active' : '' }}"><a href="/business-contact"><i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp;Support</a></li>
